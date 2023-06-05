acd209f7e2d3587ed367270af5d26fad1ce3e5f4
CipherSharma <79153600+CipherSharma@users.noreply.github.com>
2023-06-05 22:19:42 +0530

    commit message

--- AlgoBullsToDoList/wsgi.py
+++ AlgoBullsToDoList/wsgi.py
@@ -7,12 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 import os
-import sys
 
-path = os.path.expanduser('~/mysite')
-if path not in sys.path:
-    sys.path.insert(0, path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'AlgoBullsToDoList.settings'
+
+application = get_wsgi_application()
