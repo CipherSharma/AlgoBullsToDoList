1fcc4560ee05496fce654bb1e00250f8d1a99766
CipherSharma <79153600+CipherSharma@users.noreply.github.com>
2023-06-05 22:07:34 +0530

    deleting

--- home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CustomUserModel, Tag, Task
-# Register your models here.
-
-admin.site.register(CustomUserModel)
-admin.site.register(Tag)
-admin.site.register(Task)
--- home/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-from datetime import datetime
-# Create your tests here.
-date=datetime.strptime("22-09-2022", "%d-%m-%Y")
-if date<datetime.now():
-    print("false")
